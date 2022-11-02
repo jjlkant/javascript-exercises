a70c0012c6fdeb5b9ec8a15fc780dd2fca2efaa5
Jeroen Kant <jjlkant@gmail.com>
2022-11-02 23:25:39 +0100

    ENH: removeFromArray function capable of removing variable number of elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...elems) {
+    for (var i = 0; i < elems.length; i++) {
+        let index = arr.indexOf(elems[i]);
+        if (index > -1) {
+            arr.splice(index, 1)
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
