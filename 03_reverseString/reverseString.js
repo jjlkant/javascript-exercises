9c1de2ee1dd17534c336991f0d43535ae63c7e4d
Jeroen Kant <jjlkant@gmail.com>
2022-11-02 23:05:15 +0100

    ENH: Build function to reverse strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    const reverseString = inputString.split("").reverse().join("");
+    return reverseString;
 };
 
 // Do not edit below this line
