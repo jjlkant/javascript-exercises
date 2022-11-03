840488d10bd380b2e59cdf2a0a6c18b37c7cff9a
Jeroen Kant <jjlkant@gmail.com>
2022-11-03 23:42:41 +0100

    ENH: Sum all numbers between given arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(valueOne, valueTwo) {
+    if (valueOne < 0 || valueTwo < 0) {
+        return "ERROR"
+    }
+    if (!(typeof valueOne === "number") || !(typeof valueTwo === "number")) {
+        return "ERROR"
+    }
+    let start, end;
+    if (valueOne < valueTwo) {
+        start = valueOne;
+        end = valueTwo
+    } else {
+        start = valueTwo;
+        end = valueOne;
+    }
+    let sum = 0;
+    for (let i = start; i < end + 1; i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
